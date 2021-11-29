c8345232a7600ca5fd574511ebf0172ad38ca3ca
Aurélien GAUFFRE <33651103+AurelienGauffre@users.noreply.github.com>
2021-11-29 15:09:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ from utils import progress_bar
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--batch_size','-bs', default=128, type=int, help='batch size for training')
+parser.add_argument('--num_workers','-nw', default=2, type=int, help='num worker for train and test dataloader')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
+
+
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -43,12 +46,12 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=rgs.num_workers)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
