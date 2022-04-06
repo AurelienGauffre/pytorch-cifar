9926c665b360fa6a2b642f26e9931eda8a6dcf72
gauffrea <aurelien.gauffre@gmail.com>
2022-04-06 22:42:28 +0200

    Add padding

--- main.py
+++ main.py
@@ -34,35 +34,35 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # Data
 
-# random_crop = transforms.RandomCrop(32,padding=4)
-# normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
-# train_transform = transforms.Compose([
-#     random_crop,
-#     transforms.RandomHorizontalFlip(),
-#     transforms.ToTensor(),
-#     normalize,
-# ])
-#
-# test_transform = transforms.Compose([
-#     random_crop,
-#     # transforms.RandomHorizontalFlip(),
-#     transforms.ToTensor(),
-#     normalize,
-# ])
-
-
-
+random_crop = transforms.RandomCrop(32,padding=4)
+normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
 train_transform = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
+    random_crop,
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    normalize,
 ])
 
 test_transform = transforms.Compose([
+    random_crop,
+    # transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    normalize,
 ])
+
+
+
+# train_transform = transforms.Compose([
+#     transforms.RandomCrop(32, padding=4),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+# ])
+#
+# test_transform = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+# ])
 
 # Cifar
 if not args.cifar_binary:
